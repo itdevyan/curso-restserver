d003abfcfa2c034055e97e4fff8673fad8eabb62
itdevyan <itdevyan@gmail.com>
2023-06-06 17:53:38 -0400

    [Clase 112] Respaldo del RESTServer a GitHub

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -3,26 +3,34 @@
 const { request, response } = require("express");
 
 const usuariosGet = (req = request, res = response) => {
-  // (Redundante, solo para ayuda)
-  //   res.send("Hello world");
+  const query = req.query;
+  const { q, oam = "no name" } = query;
+
   res.json({
     msg: "get API - controlador",
+    query,
+    q,
+    oam,
   });
 };
 
 const usuariosPost = (req = request, res = response) => {
-  // (Redundante, solo para ayuda)
-  //   res.send("Hello world");
+  const body = req.body;
+  const { nombre, edad } = body;
   res.json({
     msg: "post API - controlador",
+    body,
+    nombre,
+    edad,
   });
 };
 
 const usuariosPut = (req = request, res = response) => {
-  // (Redundante, solo para ayuda)
-  //   res.send("Hello world");
+  const id = req.params.id;
+
   res.json({
     msg: "put API - controlador",
+    id,
   });
 };
 
